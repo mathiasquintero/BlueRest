797f5d65b6d0b0f5fab01b8396fcc48ac4160366
Mathias Quintero <me@quintero.io>
2017-08-01 17:35:39 +0200

    Updating example

--- Example/BlueRest/ViewController.swift
+++ Example/BlueRest/ViewController.swift
@@ -7,8 +7,14 @@
 //
 
 import UIKit
+import CoreBluetooth
+import BlueRest
 
 class ViewController: UIViewController {
+    
+    let uuid = CBUUID(string: "")
+    
+    var server: RESTBLEServer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +25,23 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func serve(_ sender: Any) {
+        server = RESTBLEServer.init(uuid: uuid) { (message, response) in
+            response.respond(with: "Hello World")
+        }
+        server.start()
+    }
+    
+    @IBAction func fetch(_ sender: Any) {
+        fetch(uuid: uuid, resource: "hello") { message in
+            guard let response: String = message.decode() else {
+                return
+            }
+            print(response)
+        }
+    }
+    
+    
 }
 
